97500ce02c57d92fff9d22012366b8322d3dccf5
HuyTinh <tinhnth15112003@gmail.com>
2024-09-06 15:07:08 +0700

    first commit

---
+++ backend/medication_management_service/src/main/java/com/pet_care/medication_management_service/MedicationManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pet_care.medication_management_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicationManagementServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedicationManagementServiceApplication.class, args);
+    }
+
+}
---
+++ backend/medication_management_service/src/test/java/com/pet_care/medication_management_service/MedicationManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pet_care.medication_management_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicationManagementServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
