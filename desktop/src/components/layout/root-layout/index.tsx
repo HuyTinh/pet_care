a073b0db4e8ad84ada398618fe038558ffc58418
Huy Tính <tinhnth15112003@gmail.com>
2024-10-02 16:15:42 +0700

    Delete desktop directory

--- desktop/src/components/layout/root-layout/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Outlet } from "react-router-dom";
-
-export const RootLayout = () => {
-  return (
-    <div className="h-screen bg-black overflow-hidden">
-      <Outlet />
-    </div>
-  );
-};
--- desktop/src/screens/doctor/index.tsx
+++
@@ -1,3 +0,0 @@
-export const DoctorScreen = () => {
-  return <div>Doctor Screen</div>
-}
